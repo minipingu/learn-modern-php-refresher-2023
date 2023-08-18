7764dc9704d6d62bede0f9828838da994ef5cf3b
Dede Nurhidayat <ranmaniax@gmail.com>
2023-08-18 20:31:49 +0700

    add comments

--- alternative-if-statement-syntax.php
+++ alternative-if-statement-syntax.php
@@ -1,6 +1,9 @@
-<?php $permission = 1 ?>
+one liner if
+<?php $permission = 2 ?>
 <?php if ($permission ===1) : ?>
 <h1>Hello World</h1>
-<?php elseif ($permission ===1) : ?>
+<?php elseif ($permission ===2) : ?>
 <h1>Good bye world</h1>
 <?php endif; ?>
+
+make conditional render clean, you dont need use echo echo shit in code for just showing html
