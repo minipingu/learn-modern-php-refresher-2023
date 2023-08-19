071040bbaa99f1f04acbb08d69e4006578010165
Dede Nurhidayat <ranmaniax@gmail.com>
2023-08-19 07:33:49 +0700

    7/9 oop-namespace

--- OOP/Account.php
+++ OOP/Account.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=1);
-
+//finally, i understand and create a namespace, its just virtual address or directory for class
+namespace App\Account;
 class Account {
   public function __construct(
   //Constructor Property Promotion
@@ -8,8 +9,10 @@ class Account {
   public float $balance
   ) {}
 
-  function deposit($amount){
+  public function deposit(float $amount){
     $this->balance += $amount;
+    //return for chaining method
+    return $this;
   }
 
   
--- OOP/index.php
+++ OOP/index.php
@@ -1,11 +1,9 @@
 <?php
 require_once 'Account.php';
+use namespace App/Account;
+$minipingu = new Account('minipingu', 10000);
 
-$minipingu = new Account('minipingu',10000);
-
-print_r($minipingu).'<br>' ;
-$minipingu->deposit(20000).'<br>';
-print_r($minipingu).'<br>' ;
-
-
-?>
+print_r($minipingu) . '<br>';
+// ? is nullsafe operator
+$minipingu?->deposit(20000)->deposit(10000);
+print_r($minipingu) . '<br>';
